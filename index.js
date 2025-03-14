c16df031f990d496576f9a9c4e0ff91e0bbfd2e6
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-03-14 12:33:18 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function returnsCustomerName() {
+  return customerName;
+}
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer ;
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "bob";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "not bob";
+}
+console.log(changeLeastFavoriteCustomer());
